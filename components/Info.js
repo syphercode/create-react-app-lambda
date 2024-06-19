d0d1b4e2723a88d2c2a094d36e07f48a301228a1
Brian Gibbs <122115817+syphercode@users.noreply.github.com>
2024-06-19 15:03:51 -0700

    Create Info.js

---
+++ components/Info.js
@@ -0,0 +1,13 @@
+// src/components/Info.js
+import React from 'react';
+
+const Info = () => {
+  return (
+    <div>
+      <h2>Info Page</h2>
+      <p>Information about the site</p>
+    </div>
+  );
+};
+
+export default Info;
